595d66620df8f884b9204f725332a38b8005063f
Pablo Arias <pabloarias.ic@gmail.com>
2023-09-09 21:39:54 -0300

    add pg aws config

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     ClubsModule,
   ],
