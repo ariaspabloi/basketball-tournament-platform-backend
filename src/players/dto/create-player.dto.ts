ab8021a607cf56454d52199cf60e60a5358ba02d
Pablo Arias <pabloarias.ic@gmail.com>
2023-10-17 21:21:21 -0300

    disable minLength player name

--- src/players/dto/create-player.dto.ts
+++ src/players/dto/create-player.dto.ts
@@ -1,9 +1,8 @@
-import { IsInt, Matches, MinLength } from 'class-validator';
+import { IsInt, Matches } from 'class-validator';
 import { IsOnlyDate } from '../../utils/date.validator';
 
 export class CreatePlayerDto {
   @Matches(/^[a-zA-Z]{3,}( {1,2}[a-zA-Z]{3,}){0,}$/)
-  @MinLength(20)
   name: string;
   @Matches(/^\d{7,8}-[\dkK]$/)
   rut: string;
