9aabaadf6d70a847004a707e29c68e48f7e45f1f
Pablo Arias <pabloarias.ic@gmail.com>
2023-10-17 23:12:04 -0300

    disable rut regex

--- src/players/dto/create-player.dto.ts
+++ src/players/dto/create-player.dto.ts
@@ -1,10 +1,11 @@
-import { IsInt, Matches } from 'class-validator';
+import { IsInt, IsString, Matches } from 'class-validator';
 import { IsOnlyDate } from '../../utils/date.validator';
 
 export class CreatePlayerDto {
   @Matches(/^[a-zA-Z]{3,}( {1,2}[a-zA-Z]{3,}){0,}$/)
   name: string;
-  @Matches(/^\d{7,8}-[\dkK]$/)
+  //@Matches(/^\d{7,8}-[\dkK]$/)
+  @IsString()
   rut: string;
 
   @IsOnlyDate()
