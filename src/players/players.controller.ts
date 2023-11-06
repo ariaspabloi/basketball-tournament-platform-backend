eb804d99b7e1f4c7337d77f4c6b92a57e303a758
Pablo Arias <pabloarias.ic@gmail.com>
2023-11-06 13:25:38 -0300

    add image update too players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -72,7 +72,35 @@ export class PlayersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlayerDto: UpdatePlayerDto) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './files/images',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  update(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /jpg|jpeg|png/,
+        })
+        .build({
+          fileIsRequired: false,
+        }),
+    )
+    file: Express.Multer.File,
+    @Param('id') id: string,
+    @Body() updatePlayerDto: UpdatePlayerDto,
+  ) {
+    updatePlayerDto.image = file?.filename;
     return this.playersService.update(+id, updatePlayerDto);
   }
 
