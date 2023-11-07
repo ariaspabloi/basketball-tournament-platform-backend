b3c70bee44b10afcc235bfc3cd0c6f594c2fe69a
Pablo Arias <pabloarias.ic@gmail.com>
2023-11-07 19:55:52 -0300

    add admin controller module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,14 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Role } from './entities/role.entity';
-import { ClubsController, OrganizerController } from './users.controller';
+import {
+  AdminController,
+  ClubsController,
+  OrganizerController,
+} from './users.controller';
 
 @Module({
-  controllers: [ClubsController, OrganizerController],
+  controllers: [ClubsController, OrganizerController, AdminController],
   providers: [UsersService],
   exports: [UsersService],
   imports: [TypeOrmModule.forFeature([User, Role])],
