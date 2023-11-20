61bbcadc6d26e4d3d48fd9160a855ea627cdc135
Pablo Arias <pabloarias.ic@gmail.com>
2023-11-19 22:42:40 -0300

    add excel inform

--- src/player-statistics/player-statistics.controller.ts
+++ src/player-statistics/player-statistics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Patch, Param, Get } from '@nestjs/common';
+import { Controller, Body, Patch, Param, Get, Res } from '@nestjs/common';
 import { PlayerStatisticsService } from './player-statistics.service';
 import { UpdatePlayerStatisticDto } from './dto/update-player-statistic.dto';
 
@@ -32,5 +32,27 @@ export class PlayerStatisticsController {
     @Body() updatePlayerStatisticDto: UpdatePlayerStatisticDto,
   ) {
     return this.playerStatisticsService.update(+id, updatePlayerStatisticDto);
+  }
+
+  @Get('export/:teamId/:period')
+  async exportPlayerStatistics(
+    @Param('teamId') teamId: number,
+    @Param('period') period: number,
+    @Res() res: any,
+  ) {
+    const buffer =
+      await this.playerStatisticsService.generatePlayerStatisticsExcel(
+        teamId,
+        period,
+      );
+    res.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    );
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=player-statistics.xlsx`,
+    );
+    res.end(buffer);
   }
 }
--- src/player-statistics/player-statistics.service.ts
+++ src/player-statistics/player-statistics.service.ts
@@ -4,6 +4,7 @@ import { PlayerStatistic } from './entities/player-statistic.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PlayersService } from 'src/players/players.service';
+import * as ExcelJS from 'exceljs';
 //TODO: Get all estadisticas de un partido
 @Injectable()
 export class PlayerStatisticsService {
@@ -79,5 +80,96 @@ export class PlayerStatisticsService {
       );
     await this.playerStatisticsRepository.save(playerStatistic);
     return playerStatistic;
+  }
+
+  async generateStatistics(teamId: number, period: number) {
+    const startDate = new Date();
+    startDate.setDate(startDate.getDate() - period);
+
+    // Fetching player statistics
+    const playerStatistics = await this.playerStatisticsRepository
+      .createQueryBuilder('playerStatistics')
+      .leftJoinAndSelect('playerStatistics.match', 'match')
+      .leftJoinAndSelect('playerStatistics.player', 'player')
+      .where('player.teamId = :teamId', { teamId })
+      .andWhere('match.dateTime >= :startDate', { startDate })
+      .getMany();
+
+    // Grouping statistics by player
+    const playersWithStatistics = playerStatistics.reduce((acc, stat) => {
+      const { player, ...statInfo } = stat;
+      if (!acc[player.id]) {
+        acc[player.id] = {
+          ...player,
+          statistics: [],
+        };
+      }
+      acc[player.id].statistics.push(statInfo);
+      return acc;
+    }, {});
+    // Converting the object back to an array
+    return Object.values(playersWithStatistics);
+  }
+
+  async generatePlayerStatisticsExcel(
+    teamId: number,
+    period: number,
+  ): Promise<ExcelJS.Buffer> {
+    const playersWithStatistics: any = await this.generateStatistics(
+      teamId,
+      period,
+    );
+    const workbook = new ExcelJS.Workbook();
+
+    playersWithStatistics.forEach((player) => {
+      const sheet = workbook.addWorksheet(player.name);
+
+      // Define columns for the sheet
+      sheet.columns = [
+        { header: 'Fecha', key: 'matchDate', width: 20 },
+        { header: 'Valoración', key: 'valoracion', width: 14 },
+        { header: 'Puntos Totales', key: 'totalPoints', width: 14 },
+        { header: 'Puntos', key: 'points', width: 10 },
+        { header: 'Dobles', key: 'doubleDoubles', width: 15 },
+        { header: 'Triples', key: 'threePointers', width: 15 },
+        { header: 'Faltas', key: 'fouls', width: 10 },
+        { header: 'Robos', key: 'turnovers', width: 12 },
+        { header: 'Rebotes Ofensivos', key: 'offensiveRebounds', width: 18 },
+        { header: 'Rebotes Defensivos', key: 'defensiveRebounds', width: 18 },
+        { header: 'Asistencias', key: 'assists', width: 10 },
+        { header: 'Perdidas', key: 'losses', width: 10 },
+      ];
+
+      // Add rows to the sheet
+      player.statistics.forEach((stat) => {
+        const totalPoints =
+          stat.points + stat.doubleDoubles + stat.threePointers;
+        const valoracion =
+          totalPoints +
+          stat.turnovers +
+          stat.assists +
+          stat.offensiveRebounds +
+          stat.defensiveRebounds -
+          stat.losses;
+
+        sheet.addRow({
+          matchDate: stat.match.dateTime.toISOString().split('T')[0],
+          fouls: stat.fouls,
+          points: stat.points,
+          doubleDoubles: stat.doubleDoubles,
+          threePointers: stat.threePointers,
+          turnovers: stat.turnovers,
+          offensiveRebounds: stat.offensiveRebounds,
+          defensiveRebounds: stat.defensiveRebounds,
+          assists: stat.assists,
+          losses: stat.losses,
+          totalPoints: totalPoints,
+          valoracion: valoracion,
+        });
+      });
+    });
+
+    const buffer = await workbook.xlsx.writeBuffer();
+    return buffer;
   }
 }
